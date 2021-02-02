d246be9eef310b31e4a5a1025d007afe0827a6d8
yerin <wowmtjf@naver.com>
2021-02-02 16:34:56 +0900

    수정

--- GURU2_Todolist/app/src/main/java/com/example/guru2_todolist/MainActivity.kt
+++ GURU2_Todolist/app/src/main/java/com/example/guru2_todolist/MainActivity.kt
@@ -7,6 +7,6 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        //수정gURU
+        //박예린
     }
 }
